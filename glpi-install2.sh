dd47da743e0b4bf8a99f68957fc14069142a4ff8
JuGuillot <161329881+JuGuillot@users.noreply.github.com>
2024-05-24 09:08:43 +0200

    Add files via upload

--- glpi-install2.sh
+++ glpi-install2.sh
@@ -1,127 +1,98 @@
 #!/bin/bash
 
-function install_packages() {
-        info "Installing packages..."
-        sleep 1
-        apt update
-        apache2 \
-                mariadb-server \
-                perl \
-                curl \
-                jq \
-                php \
-                php-ldap \
-                php-imap \
-                php-apcu \
-                php-xmlrpc \
-                php-cas \
-                php-mysqli \
-                php-mbstring \
-                php-curl \
-                php-gd \
-                php-simplexml \
-                php-xml \
-                php-intl \
-                php-zip \
-                php-bz2
-        systemctl enable mariadb
-        systemctl enable apache2
+# Lire les informations de configuration à partir du fichier texte
+db_name=$(grep "^db_name" config.txt | cut -d "=" -f 2 | xargs)                     # Nom de la BDD
+db_user=$(grep "^db_user" config.txt | cut -d "=" -f 2 | xargs)                     # Utilisateur BDD
+db_pass=$(grep "^db_pass" config.txt | cut -d "=" -f 2 | xargs)                     # Mot de passe BDD
+network_interface=$(grep "^network_interface" config.txt | cut -d "=" -f 2 | xargs) # Interface réseau
+
+# Installation des paquets nécessaires
+apt update && apt upgrade -y
+apt install php-xml php-common php-json php-mysql php-mbstring php-curl php-gd php-intl php-zip php-bz2 php-imap php-apcu -y
+apt install apache2 php mariadb-server -y
+
+# Sécurisation de l'installation MariaDB
+mysql_secure_installation
+
+# Création de la base de données MySQL
+mysql -e "CREATE DATABASE $db_name"
+mysql -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'localhost' IDENTIFIED BY '$db_pass'"
+mysql -e "FLUSH PRIVILEGES"
+
+# Télécharger et extraire GLPI
+cd /tmp
+wget https://github.com/glpi-project/glpi/releases/download/10.0.15/glpi-10.0.15.tgz
+tar -xzvf glpi-10.0.15.tgz -C /var/www/
+
+# Attribuer les permissions
+chown www-data /var/www/glpi/ -R
+
+# Création des dossiers nécessaires
+mkdir /etc/glpi
+chown www-data /etc/glpi/
+mv /var/www/glpi/config /etc/glpi
+
+mkdir /var/lib/glpi
+chown www-data /var/lib/glpi/
+mv /var/www/glpi/files /var/lib/glpi
+
+mkdir /var/log/glpi
+chown www-data /var/log/glpi
+
+# Création des fichiers de configuration PHP
+touch /var/www/glpi/inc/downstream.php
+cat > /var/www/glpi/inc/downstream.php <<EOF
+<?php
+define('GLPI_CONFIG_DIR', '/etc/glpi/');
+if (file_exists(GLPI_CONFIG_DIR . '/local_define.php')) {
+    require_once GLPI_CONFIG_DIR . '/local_define.php';
 }
+EOF
 
-function mariadb_configure() {
-        systemctl start mariadb
+touch /etc/glpi/local_define.php
+cat > /etc/glpi/local_define.php <<EOF
+<?php
+define('GLPI_VAR_DIR', '/var/lib/glpi/files');
+define('GLPI_LOG_DIR', '/var/log/glpi');
+EOF
 
-        # Set the root password
-        mysql -e "UPDATE mysql.user SET Password = PASSWORD('Azerty1*') WHERE User = 'root'"
-        # Remove anonymous user accounts
-        mysql -e "DELETE FROM mysql.user WHERE User = ''"
-        # Disable remote root login
-        mysql -e "DELETE FROM mysql.user WHERE User = 'root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
-        # Remove the test database
-        mysql -e "DROP DATABASE test"
-        # Reload privileges
-        mysql -e "FLUSH PRIVILEGES"
-        # Create a new database
-        mysql -e "CREATE DATABASE glpi"
-        # Create a new user
-        mysql -e "CREATE USER 'glpi_user'@'localhost' IDENTIFIED BY 'Azerty1*'"
-        # Grant privileges to the new user for the new database
-        mysql -e "GRANT ALL PRIVILEGES ON glpi.* TO 'glpi_user'@'localhost'"
-        # Reload privileges
-        mysql -e "FLUSH PRIVILEGES"
-}
-
-function install_glpi() {
-        info "Downloading and installing the latest version of GLPI..."
-        # Get download link for the latest release
-        DOWNLOADLINK=$(curl -s https://api.github.com/repos/glpi-project/glpi/releases/latest | jq -r '.assets[0].browser_download_url')
-        wget -O /tmp/glpi-latest.tgz $DOWNLOADLINK
-        tar xzf /tmp/glpi-latest.tgz -C /var/www/html/
-
-        # Add permissions
-        chown -R www-data:www-data /var/www/html/glpi
-        chmod -R 775 /var/www/html/glpi
-
-        # Setup vhost
-        cat >/etc/apache2/sites-available/000-default.conf <<EOF
+# Configuration Apache2 pour GLPI
+touch /etc/apache2/sites-available/support.pharmgreen.org.conf
+cat > /etc/apache2/sites-available/support.pharmgreen.org.conf <<EOF
 <VirtualHost *:80>
-       DocumentRoot /var/www/html/glpi/public  
-       <Directory /var/www/html/glpi/public>
-                Require all granted
-                RewriteEngine On
-                RewriteCond %{REQUEST_FILENAME} !-f
-                RewriteRule ^(.*)$ index.php [QSA,L]
-        </Directory>
-        
-        LogLevel warn
-        ErrorLog \${APACHE_LOG_DIR}/error-glpi.log
-        CustomLog \${APACHE_LOG_DIR}/access-glpi.log combined
-        
+    ServerName pharmgreen.org
+
+    DocumentRoot /var/www/glpi/public
+
+    <Directory /var/www/glpi/public>
+        Require all granted
+
+        RewriteEngine On
+
+        RewriteCond %{REQUEST_FILENAME} !-f
+        RewriteRule ^(.*)$ index.php [QSA,L]
+    </Directory>
+    <FilesMatch \.php$>
+        SetHandler "proxy:unix:/run/php/php8.2-fpm.sock|fcgi://localhost/"
+    </FilesMatch>
 </VirtualHost>
 EOF
 
-        #Disable Apache Web Server Signature
-        echo "ServerSignature Off" >>/etc/apache2/apache2.conf
-        echo "ServerTokens Prod" >>/etc/apache2/apache2.conf
+# Activer le site GLPI et modules Apache
+a2ensite support.pharmgreen.org
+a2dissite 000-default.conf
+a2enmod rewrite
 
-        # Setup Cron task
-        echo "*/2 * * * * www-data /usr/bin/php /var/www/html/glpi/front/cron.php &>/dev/null" >>/etc/cron.d/glpi
+# Redémarrer Apache
+systemctl restart apache2
 
-        #Activation du module rewrite d'apache
-        a2enmod rewrite && systemctl restart apache2
-}
+# Installation et configuration de PHP-FPM
+apt-get install php8.2-fpm -y
+sudo a2enmod proxy_fcgi setenvif
+sudo a2enconf php8.2-fpm
+sudo systemctl reload apache2
+sed -i 's/^\(session\.cookie_httponly\s*=\s*\).*/\1on/' /etc/php/8.2/fpm/php.ini
 
-function setup_db() {
-        info "Setting up GLPI..."
-        cd /var/www/html/glpi
-        php bin/console db:install --db-name=pharmgreen.glpi --db-user=glpi_user --db-password=Azerty1* --no-interaction
-        rm -rf /var/www/html/glpi/install
-}
-
-function display_credentials() {
-        info "=======> GLPI installation details  <======="
-        warn "It is important to record this informations. If you lose them, they will be unrecoverable."
-        info "==> GLPI:"
-        info "Default user accounts are:"
-        info "USER       -  PASSWORD       -  ACCESS"
-        info "glpi       -  glpi           -  admin account,"
-        info "tech       -  tech           -  technical account,"
-        info "normal     -  normal         -  normal account,"
-        info "post-only  -  postonly       -  post-only account."
-        echo ""
-        info "You can connect access GLPI web page from IP or hostname:"
-        info "http://$IPADRESS or http://$HOST"
-        echo ""
-        info "==> Database:"
-        info "root password:           Azerty1*"
-        info "glpi_user password:      Azerty1*"
-        info "GLPI database name:          pharmgreen.glpi"
-        info "<==========================================>"
-        echo ""
-}
-
-install_packages
-mariadb_configure
-install_glpi
-setup_db
-display_credentials
+# Redémarrer PHP-FPM et Apache
+systemctl restart php8.2-fpm.service
+sudo systemctl restart apache2
